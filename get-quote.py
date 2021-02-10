7beda88324ddd949f5a9b855eb115718be4af30c
heidiphn <36883833+heidiphn@users.noreply.github.com>
2021-02-11 00:46:59 +0100

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,16 +1,14 @@
 def primary():
-    print("Keep it logically awesome.")
+    # print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  print(quotes[0])
 
 if __name__== "__main__":
   primary()
 
 
-    git add get-quote.py
-    git commit -m "Renamed the primary function"
-    git push
+  
